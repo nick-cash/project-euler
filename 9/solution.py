4b45d3486b7bd8bdc1b9027c144a102acb2e190e
Nick Cash <nicholas.a.cash@gmail.com>
2014-06-04 22:20:32 -0500

    Solution to Problem 9

---
+++ 9/solution.py
@@ -0,0 +1,13 @@
+def is_triplet(number_list):
+    if (a < b) and (b < c):
+        return ((a * a) + (b * b)) == (c * c)
+    return False
+
+
+for a in xrange(3, int(1000 / 3)):
+    for b in xrange(a+1, (1000 - a) / 2):
+        c = 1000 - a - b
+
+        if is_triplet([a, b, c]):
+            print a, b, c
+            print int(a * b * c)
